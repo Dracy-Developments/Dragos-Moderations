940a026f0c6df0f367909ec753d756c23846003b
Drago <yadiel.diazjimenez@outlook.com>
2020-05-17 21:09:20 -0500

    Made my first fucking mistake again

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { Client } = require (`discord.js`);
+const client = new Client();
+const { token } = require(`config.json`);
+
+client.on(ready, () => {
+    console.log(`The Bot is Ready!`)
+})
+
+client.login(token)
