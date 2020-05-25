1c2896415fb48f7fd7debaf70d03d6373bd9ab83
Drago <yadiel.diazjimenez@Outlook.com>
2020-05-24 20:39:24 -0500

    Added a Message Event

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const { Client } = require (`discord.js`);
 const client = new Client();
-const { token } = require(`../config.json`);
+const { token, prefix } = require(`../config.json`);
 
 client.on("ready", () => {
     console.log(`The Bot is Ready!`)
 })
+
+client.on("message", message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd === "test"){
+        message.channel.send(`The Command Works`)
+    }
+})
+
 
 client.login(token)
