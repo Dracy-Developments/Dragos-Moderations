7be55cf485466dca40d36785bac269d6bdafdc19
Drago <yadiel.diazjimenez@outlook.com>
2020-05-28 22:38:02 -0500

    added a test command

---
+++ commands/devs/test.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require(`discord.js`)
+module.exports = {
+    name: "test",
+    category: "devs",
+    description: "Test Command",
+    usage: "<String>",
+    aliases: [``],
+    run: async (client, message, args) => {
+        //Insert Shit Here
+    }
+}
