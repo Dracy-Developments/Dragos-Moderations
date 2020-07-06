cc0e521fe49bcf2bf2f5a2bec73b5e086606d188
Jason <jdragon@lucicode.eu>
2020-07-07 00:26:51 +0200

    I built a foking boat it's time to sail to the .js

--- models/settings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const settingsSchema = mongoose.Schema({
-    guildID: String,
-    muteRole: String,
-    strikeToggle: Boolean,
-    maxStrike: Number,
-    prefix: String,
-});
-module.exports = mongoose.model('Settings', settingsSchema);
